70e7f901f1e221147c0cf3a5e5a7e1bff73cd239
syah255 <73102858+syah255@users.noreply.github.com>
2021-01-19 00:37:57 +0800

    Update clientsnake.py

--- clientsnake.py
+++ clientsnake.py
@@ -1,5 +1,4 @@
 import socket
-import math
 import errno
 import time
 import random
@@ -16,12 +15,11 @@ nickname = input("Choose your nickname: ")
 
 # Connecting To Server
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-client.connect(('192.168.1.15', 8888))
+client.connect(('192.168.1.15', 55555))
 
-# effects! add a delay of 1 second before performing any action
-WAIT = 1
+# just of effects. add a delay of 1 second before performing any action
+WAIT  = 1
 MAX_VAL = 50
-DICE_FACE = 6
 
 # snake takes you down
 snakes = {
@@ -37,6 +35,7 @@ ladders = {
     20: 41,
 }
 
+
 player_turn_text = [
     "Your turn.",
     "Go.",
@@ -44,6 +43,15 @@ player_turn_text = [
     "Lets win this.",
     "Are you ready?",
     "",
+]
+
+
+snake_bite = [
+    "Astagfirullah",
+    "OMG !",
+    "you got snake bite",
+    "oh no !!",
+    "auchhhhhh "
 ]
 
 
@@ -56,11 +64,11 @@ ladder_jump = [
 ]
 
 
+
 def welcome_msg():
     msg = """
     Welcome to Snake and Ladder Game.
- 
-    
+
     41 42 43 44 45 46 47 48 49 50
     ^     ^        *        *
     40 39 38 37 36 35 34 33 32 31
@@ -75,17 +83,12 @@ def welcome_msg():
     49->13   20->41
     46->14   9->30
     39->3    5->43
-
-
-  
     """
     print(msg)
 
-
-
 def get_dice_value():
     time.sleep(WAIT)
-    dice_value = random.randint(1, DICE_FACE)
+    dice_value = random.randint(1,6)
     print("Its a " + str(dice_value))
     return dice_value
 
@@ -99,13 +102,15 @@ def got_ladder_jump(old_value, current_value, player_name):
     print("\n" + player_name + " climbed the ladder from " + str(old_value) + " to " + str(current_value))
 
 
+
+
 def snake_ladder(player_name, current_value, dice_value):
     time.sleep(WAIT)
     old_value = current_value
     current_value = current_value + dice_value
 
     if current_value > MAX_VAL:
-        print("You need " + str(MAX_VAL - old_value) + " to win this game. Keep trying,OKAY >
+        print("You need " + str(MAX_VAL - old_value) + " to win this game. Keep trying,OKAY !" )
         return old_value
 
     print("\n" + player_name + " moved from " + str(old_value) + " to " + str(current_value))
@@ -123,18 +128,17 @@ def snake_ladder(player_name, current_value, dice_value):
     return final_value
 
 
-
 def check_win(player_name, position):
     time.sleep(WAIT)
     if MAX_VAL == position:
         print("\n\n\nThats it.\n\n" + player_name + " won the game.")
         print("Congratulations " + player_name)
         print("\nThank you for playing the game.\n\n")
-        sys.exit(1)
+        again=input("\nDo you want to play again? (y/n) ")
+        if again == 'n':
+           sys.exit(1)
 
-
-
-# startgame()
+#gamestart
 def startgame():
     while True:
         try:
@@ -144,38 +148,42 @@ def startgame():
             if message == 'NICK':
                 client.send(nickname.encode('ascii'))
             else:
-                 time.sleep(SLEEP_BETWEEN_ACTIONS)
+                 time.sleep(WAIT)
                  player_name = nickname
-                 time.sleep(SLEEP_BETWEEN_ACTIONS)
+                 time.sleep(WAIT)
                  player_current_position = 0
-                 round=0
+                 round = 0
                  while True:
                       welcome_msg()
-                      time.sleep(WAIT)                  
+                      time.sleep(WAIT)
                       print("Round:")
                       print(round)
                       print("\nCurrent position :")
                       print(player_current_position)
-                      input_1 = input("\n" + player1_name + ": " + random.choice(player_turn_text) + " Hit the enter to roll dice: ")
+                      input_1 = input("\n" + player_name + ": " + random.choice(player_turn_text) + " Hit the enter to roll dice: ")
                       print("\nRolling dice...")
                       dice_value = get_dice_value()
                       time.sleep(WAIT)
                       print(player_name + " moving....")
-                      player_current_position = snake_ladder(player1_name, player1_current_position, dice_value)
+                      player_current_position = snake_ladder(player_name, player_current_position, dice_value)
                       time.sleep(3)
                       round=round+1
+#                     client.send(bytes(player_current_position,'ascii'))
 #                     sent name and current position
 #                     client.send(player_name,player_current_position.encode('ascii'))
-              
-                      check_win(player1_name, player_current_position)
-                       _ = system('clear')
 
+
+                      check_win(player_name, player_current_position)
+
+                      _ = system('clear')
 
         except socket.error as e:
-            # Close Connection When Error
-            print(str(e))
-            sys.exit()
+           print (str(e))
+           sys.exit()
 
 
 startgame()
+
+
+
 
